215c4f09fc6c7b90586a662e1339ad5d31c05cbd
Mohammad Sharifi <mmdsharifi@yahoo.com>
2015-02-13 19:03:05 +0330

    Init commit design

---
+++ application/views/master_view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+</head>
+<body>
+	<header></header>
+	<main>
+		<p>Welcome to our site</p>
+	</main>
+	<footer></footer>
+</body>
+</html>

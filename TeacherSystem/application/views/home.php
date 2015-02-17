123d488e948635cb56a7dc0cb12a5fb5c39edc6e
Mohammad Sharifi <mmdsharifi@yahoo.com>
2015-02-17 23:06:06 +0330

    updated and messages added

---
+++ TeacherSystem/application/views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <p>Welcome to our site</p>
+    <a href="<?php echo $newMessUrl; ?>">New Message</a>
+</body>
+</html>

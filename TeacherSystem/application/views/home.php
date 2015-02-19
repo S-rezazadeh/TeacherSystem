3cd86e2192a34c44089f1fa2ba66c9f7216ebe6e
Mohammad Sharifi <mmdsharifi@yahoo.com>
2015-02-19 14:03:12 +0330

    icon fonts added

--- TeacherSystem/application/views/home.php
+++ TeacherSystem/application/views/home.php
@@ -5,7 +5,28 @@
     <title>Document</title>
 </head>
 <body>
-    <p>Welcome to our site</p>
-    <a href="<?php echo $newMessUrl; ?>">New Message</a>
+	<main>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h3>خوش آمدید به این سایت</h3>
+					<div class="well well-sm">
+						<div class="row">
+							<div class="col-md-4">
+								<figure>
+									<span class="icon-ion-android-archive"></span>
+								<figcaption>
+									سوالات خود را با استاد در میان بگذارید
+								</figcaption>
+								</figure>
+							</div>
+							<div class="col-md-4"></div>
+							<div class="col-md-4"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
 </body>
 </html>

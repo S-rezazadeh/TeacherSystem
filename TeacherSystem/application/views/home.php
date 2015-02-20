e0ff1cb24cac7f32a587ac4fd14c20c4eedbdce0
Mohammad Sharifi <mmdsharifi@yahoo.com>
2015-02-20 10:06:58 +0330

    main-page with register

--- TeacherSystem/application/views/home.php
+++ TeacherSystem/application/views/home.php
@@ -1,32 +1,104 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-	<main>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<h3>خوش آمدید به این سایت</h3>
-					<div class="well well-sm">
-						<div class="row">
-							<div class="col-md-4">
-								<figure>
-									<span class="icon-ion-android-archive"></span>
-								<figcaption>
-									سوالات خود را با استاد در میان بگذارید
-								</figcaption>
-								</figure>
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+	</head>
+	<body>
+		<main>
+			<section class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h3>خوش آمدید به این سایت</h3>
+						<div class="well well-sm">
+							<div class="row">
+								<div class="col-md-4">
+									<figure class="feature">
+										<span class="icon-question-answer"></span>
+										<figcaption>
+											سوالات خود را با استاد در میان بگذارید
+											سوالات خود را با استاد در میان بگذارید
+										</figcaption>
+									</figure>
+								</div>
+								<div class="col-md-4">
+									<figure class="feature">
+										<i class="icon-new-releases"></i>
+										<figcaption>
+											سوالات خود را با استاد در میان بگذارید
+											سوالات خود را با استاد در میان بگذارید
+										</figcaption>
+									</figure>
+								</div>
+								<div class="col-md-4">
+									<figure class="feature">
+										<span class="icon-file-download"></span>
+										<figcaption>
+											سوالات خود را با استاد در میان بگذارید
+											سوالات خود را با استاد در میان بگذارید
+										</figcaption>
+									</figure>
+								</div>
+
 							</div>
-							<div class="col-md-4"></div>
-							<div class="col-md-4"></div>
+						</div>
+						<div role="tabpanel">
+
+							<!-- Nav tabs -->
+							<ul class="nav nav-tabs" role="tablist">
+								<li role="presentation"><a href="#login"
+														   aria-controls="profile" role="tab" data-toggle="tab">ورود به سایت</a></li>
+								<li role="presentation" class="active"><a href="#register"
+																		  aria-controls="home" role="tab" data-toggle="tab">ثبت نام در سایت</a></li>
+							</ul>
+
+							<!-- Tab panes -->
+							<div class="tab-content">
+								<div role="tabpanel" class="tab-pane" id="login">...</div>
+								<div role="tabpanel" class="tab-pane active row" id="register">
+									<form class="col-md-6">
+										<div class="form-group">
+											<label for="username">نام و نام خانوادگی</label>
+											<input type="text" class="form-control"
+											id="username" placeholder="نام کاربری را وارد کنید">
+										</div>
+										<div class="form-group">
+											<label for="email">ایمیل</label>
+											<input type="email" class="form-control"
+												   id="email" placeholder="ایمیل را وارد کنید">
+										</div>
+										<div class="form-group">
+											<label for="pass">گذر واژه</label>
+											<input type="password" class="form-control"
+												   id="pass" placeholder="گذرواژه را وارد کنید">
+										</div>
+										<div class="form-group">
+											<label for="pass">دانشگاه</label>
+											<select class="form-control">
+												<option>دانشگاه صنعتی ارومیه</option>
+												<option>دانشگاه سما</option>
+											</select>
+										</div>
+										<button type="submit"
+										class="btn btn-default" id="register-btn">ثبت نام</button>
+									</form>
+									<div class="info">
+									<section>
+									<h3>ٰروند ثبت نام شما در سیستم</h3>
+										<p>
+											یکسری اطلاعات در مورد ثبت نام در اینجا قرار گیرد و دانشجو را با فرایند عضویت او در سیستم آشنا کند.
+										</p>
+									</section>
+
+									</div>
+								</div>
+
+							</div>
+
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	</main>
-</body>
+			</section>
+		</main>
+	</body>
 </html>

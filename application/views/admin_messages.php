070f0a9a1762ab73bedba7eb351f55c9922c638c
Mohammad Sharifi <mmdsharifi@yahoo.com>
2015-03-03 23:25:11 +0330

    Admin message added

--- application/views/admin_messages.php
+++ application/views/admin_messages.php
@@ -1,1 +1,133 @@
-<div>Messages</div>
+<section class="panel panel-default">
+	<div class="panel-heading">
+		<h1 class="panel-title">لیست پیغام ها</h1>
+	</div>
+	<div class="panel-body">
+		<div class="col-md-4">
+			<div class="list-group">
+				<a href="#" class="list-group-item active  unread">محمد شریفی
+					<small>سلام استاد...</small><time class="pull-right badge">امروز</time>
+				</a>
+				<a href="#" class="list-group-item">محمد شریفی
+					<small>سلام استاد...</small><time class="pull-right badge">امروز</time>
+				</a>
+				<a href="#" class="list-group-item">سعید پیری
+					<small>سلام استاد...</small><time class="pull-right badge">امروز</time>
+				</a>
+				<a href="#" class="list-group-item unread">علی محمدی
+					<small>در مورد اسلاید های شی گرایی</small><time class="pull-right badge">امروز</time>
+				</a>
+				<a href="#" class="list-group-item">رضازاده
+					<small>سلام استاد...</small><time class="pull-right badge">14 دی</time>
+				</a>
+			</div>
+		</div>
+		<div class="col-md-8">
+			<div class="list-group">
+				<div id="body-msg" class="list-group-item">
+					<div class="msg-item panel panel-default">
+						<div class="panel-heading">
+							<h5>محمدشریفی <small class="pull-right">دانشگاه صنعتی ارومیه</small></h5>
+						</div>
+						<div class="panel-body message-body">
+							<p>
+
+								متن پیغام برای محمد شریفی است و ممکن است خیلی طولانی باشد بحدی که حوصله استاد را سر ببرد و استاد در جوابش فقط بنویسد که نه نمی شود.!!
+
+							</p>
+							<img src="<?php echo base_url(); ?>assets/img/1.jpg" alt="" class="img-responsive">
+							<p>
+							<hr>
+								<span>
+								<span class="icon-access-time pull-left"></span>
+									<span>15:23</span>
+								</span>
+								<time class="pull-right">
+									<span class="icon-ion-calendar pull-left"></span>
+									<span>12 بهمن</span>
+								</time>
+							</p>
+						</div>
+						<div class="panel-footer ">
+						<div class="row">
+							<div class="dl-attachments col-xs-6">
+
+								<a href="#" class="btn btn-default btn-sm">دانلود فایل پیوست
+									<span class=" icon-file-download text-primary"></span>
+								</a>
+							</div>
+							<div class="seen col-xs-6 ">
+								<div class="btn btn-sm pull-right">
+									<span class="icon-done-all text-success"></span>
+									<span>دیده شده</span>
+								</div>
+							</div>
+						 </div>
+						</div>
+					</div>
+				<div class="msg-item panel panel-default">
+					<div class="panel-heading">
+						<h5>محمدشریفی <small class="pull-right">دانشگاه صنعتی ارومیه</small></h5>
+					</div>
+					<div class="panel-body message-body">
+						<p>
+
+							متن پیغام برای محمد شریفی است و ممکن است خیلی طولانی باشد بحدی که حوصله استاد را سر ببرد و استاد در جوابش فقط بنویسد که نه نمی شود.!!
+
+						</p>
+						<img src="<?php echo base_url(); ?>assets/img/1.jpg" alt="" class="img-responsive">
+						<p>
+						<hr>
+						<span>
+							<span class="icon-access-time pull-left"></span>
+							<span>15:23</span>
+						</span>
+						<time class="pull-right">
+							<span class="icon-ion-calendar pull-left"></span>
+							<span>12 بهمن</span>
+						</time>
+						</p>
+				</div>
+				<div class="panel-footer ">
+					<div class="row">
+						<div class="dl-attachments col-xs-6">
+
+							<a href="#" class="btn btn-default btn-sm">دانلود فایل پیوست
+								<span class=" icon-file-download text-primary"></span>
+							</a>
+						</div>
+						<div class="seen col-xs-6 ">
+							<div class="btn btn-sm pull-right">
+								<span class="icon-done-all text-success"></span>
+								<span>دیده شده</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="panel-footer">
+	<div class="row">
+		<div class="col-md-offset-4 col-lg-8">
+			<div class="input-group">
+				<span class="input-group-btn">
+					<button class="btn btn-default" type="button">
+						<sapn class="icon-add-to-photos"></sapn>
+						<span>ضمیمه کردن فایل پیوست</span>
+					</button>
+				</span>
+				<input type="text" class="form-control" placeholder="پاسخ خود را اینجا بنویسید...">
+				<span class="input-group-btn">
+					<button class="btn btn-primary" type="button">
+						<sapn class="icon-ion-android-send"></sapn>
+						<span>ارسال</span>
+					</button>
+				</span>
+			</div><!-- /input-group -->
+		</div><!-- /.col-lg-6 -->
+</div>
+	</div>
+</section>

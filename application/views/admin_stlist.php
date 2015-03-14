7e58bbc3b89d113c6a1aae721633558f06fda5a6
sahar <saharrezazadeh96@yahoo.com>
2015-03-14 21:01:23 +0330

    stlistY

--- application/views/admin_stlist.php
+++ application/views/admin_stlist.php
@@ -1,1 +1,68 @@
-<div>Student List</div>
+    <div class="container">
+        
+        <div class="panel panel-default">
+           <div class="panel-heading">
+<h1 class="panel-title">لیست پیغام ها</h1>
+</div>
+  <div class="panel-body">
+    <div class="table-responsive">
+         <table class="table table-hover table-striped">
+             <tr class="active">
+                 <td class="col-md-1" style="background-color:darkgray  "></td>
+                 <td class="col-md-3" style="background-color:darkgray">نام دانشجو</td>
+                 <td class="col-md-3" style="background-color:darkgray">دانشگاه</td>
+                 <td class="col-sm-2" style="background-color:darkgray" >وضعیت تایید ایمیل</td>
+                 <td class="col-md-1" style="background-color:darkgray">تایید استاد</td>
+                 <td class="col-md-1" style="background-color:darkgray">عملیات</td>               
+             </tr>
+             
+             <tr>
+                 <td>1</td>
+                 <td>فاطمه علیپور</td>
+                 <td>  
+                  صنعتی ارومیه
+                 </td>
+                 <td>
+                   <button class="btn btn-default btn-sm" aria-label="Left Align">
+                       	<span class="icon-ion-android-cancel text-danger" style="font-size:20px">
+                 </td>
+                 <td>  <button class="btn btn-default btn-sm" aria-label="Left Align">
+                       	<span class="icon-ion-checkmark-circled text-success"style="font-size:20px">
+                     </td>
+                 <td>       
+                         <button class="btn btn-default btn-sm" aria-label="Left Align">
+                       	<span class="icon-ion-android-cancel text-danger" style="font-size:20px">
+                          <button class="btn btn-default btn-sm" aria-label="Left Align">
+                       <span class="icon-ion-android-delete" style="font-size:20px">  
+               
+                     </td>
+            </tr>
+                          <tr>
+                 <td>2</td>
+                 <td>علی صادقی</td>
+                 <td>  
+                  ازاد ارومیه
+                 </td>
+                 <td>
+                   <button class="btn btn-default btn-sm" aria-label="Left Align">
+                       	<span class="icon-ion-android-cancel text-danger" style="font-size:20px">
+                 </td>
+                 <td>  <button class="btn btn-default btn-sm" aria-label="Left Align">
+                       	<span class="icon-ion-android-cancel text-danger" style="font-size:20px">
+                     </td>
+                 <td>       
+                         <button class="btn btn-default btn-sm" aria-label="Left Align">
+                       	<span class="icon-ion-android-cancel text-danger" style="font-size:20px">
+                          <button class="btn btn-default btn-sm" aria-label="Left Align">
+                       <span class="icon-ion-android-delete" style="font-size:20px">  
+               
+                     </td>
+            </tr>               
+                            
+          </table>
+             
+             
+</div>
+                       </div>
+</div>
+        </div>
